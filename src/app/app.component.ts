308f9c7b65fd3d5cb8c4295127dce08eac4ac6de
Guest User <Guest@Epicodus-5A.local>
2018-03-26 11:23:17 -0700

    add Recipe editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import {Recipe} from './models/recipe.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'Recipe Box';
+  recipes: Recipe[] = [
+    new Recipe('Apple pie', 'Grandmas recipe of a delicious apple pie', 'Instructions......'),
+    new Recipe('Quesadilla', 'Delicious chicken Quesadilla', 'Instructions......'),
+    new Recipe('Pumpkin pie', 'Family recipe of a delicious pumpkin pie', 'Instructions......')
+  ];
+
+  selectedRecipe = null;
+
+  editRecipe(clickedRecipe) {
+    this.selectedRecipe = clickedRecipe;
+  }
+
+  finishedEditing() {
+    this.selectedRecipe = null;
+  }
+
 }
